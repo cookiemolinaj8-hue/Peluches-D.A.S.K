696fe1d7f79a5d231f1e299bc28825b66c9a4877
Kevinx-star <kevinandrespiernagorda@gmail.com>
2026-06-06 15:27:59 -0500

    Se crean los archivos producto, footer y header en formato .php para la pagina web de peluches D.A.S.K

---
+++ peluches D.A.S.K/includes/footer.php
@@ -0,0 +1,24 @@
+<section class="contacto">
+    <h2>Contacto</h2>
+    <div class="info-contacto">
+        <p>📍 Calle 54 F # 43 A 76 SUR</p>
+        <p>📞 300 123 4567</p>
+        <p>📧 peluchesDASK@gmail.com</p>
+    </div>
+
+    <h3>Síguenos</h3>
+    <div class="redes">
+        <a href="https://facebook.com" target="_blank" class="btn facebook">
+            <i class="fab fa-facebook-f"></i>
+        </a>
+        <a href="https://instagram.com" target="_blank" class="btn instagram">
+            <i class="fab fa-instagram"></i>
+        </a>
+        <a href="https://tiktok.com" target="_blank" class="btn tiktok">
+            <i class="fab fa-tiktok"></i>
+        </a>
+    </div>
+</section>
+
+</body>
+</html>
