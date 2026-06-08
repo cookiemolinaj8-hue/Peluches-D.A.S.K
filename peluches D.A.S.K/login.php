49e1c7912d10a0c0e1516b43555d75b453d2381d
Kevinx-star <kevinandrespiernagorda@gmail.com>
2026-06-08 18:44:10 -0500

    Se sube el archivo "login.php" funcionando

---
+++ peluches D.A.S.K/login.php
@@ -0,0 +1,48 @@
+<?php
+
+$servidor = "localhost";
+$usuario = "root";
+$contraseña = "";
+$base_datos = "peluches-dask";
+
+$enlace = mysqli_connect ($servidor, $usuario, $contraseña, $base_datos);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    
+    <form class = "login" action="#" method="post">
+
+    <input type="text" name="nombre" placeholder="Nombre">
+    <input type="email" name="correo" placeholder="Correo">
+    <input type="text" name="telefono" placeholder="Teléfono">
+
+    <input type="submit" name="registro">
+    <input type="reset">
+
+    </form>
+
+</body>
+</html>
+
+<?php
+
+if (isset($_POST['registro'])){
+
+    $nombre = $_POST['nombre'];
+    $correo = $_POST['correo'];
+    $telefono = $_POST['telefono'];
+
+    $insertarDatos = "INSERT INTO login VALUES('$nombre','$correo','$telefono','')";
+
+    $ejecutarInsertar = mysqli_query ($enlace, $insertarDatos);
+
+}    
+?>
